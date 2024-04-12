a3fb7f4565e250ab039a1bf9671ebe4732142c30
Fernanda López <fer.daye.2007@gmail.com>
2024-04-11 21:29:37 -0600

    Header

--- Peliculas/src/App.jsx
+++ Peliculas/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route}  from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Header from './components/Header'
 const App = () => {
   return (
     <>
@@ -10,6 +11,7 @@ const App = () => {
       <div className='container'>
       <Routes>
         {/* aqui estan las rutas */}
+        <Route path='/h' element={<Header/>}/>
         <Route path= '/' element={<Dashboard/>}/>
         <Route path= '/login' element={<Login/>}/>
         <Route path= '/register' element={<Register/>}/>
---
+++ Peliculas/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import {  } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <>
+        <header>
+<h1>
+  hola estrellitas, el mundo les dice hola
+</h1>
+
+        </header>
+</>
+)}
+
+export default Header
