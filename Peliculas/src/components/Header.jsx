5cb0445dad2cbf7bb4ea24e7db10b335f8718a3e
Fernanda López <fer.daye.2007@gmail.com>
2024-04-12 20:56:47 -0600

    NavBar Diseñado💥

--- Peliculas/src/components/Header.jsx
+++ Peliculas/src/components/Header.jsx
@@ -1,13 +1,26 @@
-import {  } from "react-icons/fa";
+import { FaUser,  FaUserPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
         <header>
-<h1>
-  hola estrellitas, el mundo les dice hola
-</h1>
+    <div className="headerContenedor">
 
+      <div className="contenedorA">
+        <img src="https://www.flaticon.es/icono-gratis/letra-p_3665997?term=p&page=1&position=3&origin=search&related_id=3665997" alt="" />
+      </div>
+
+      <input className="contenedorB" type="text" id="s" value="" placeholder="Buscar"  />
+      
+      <div>
+          <Link className="contenedorA" to='/login'> <FaUser/> Login </Link>
+        </div>
+      
+      <div >
+      <Link className="contenedorA" to='/register'> <FaUserPlus/> Registrer </Link>
+        </div>
+      </div>
         </header>
 </>
 )}
