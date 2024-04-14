696972b786ea7dd0ebfed43c664423a54b104ed5
Fernanda López <fer.daye.2007@gmail.com>
2024-04-13 18:07:21 -0600

    Ajustando la estructura para empezar con la conexion

--- Peliculas/src/pages/Register.jsx
+++ Peliculas/src/pages/Register.jsx
@@ -1,6 +1,19 @@
 import { FaUser } from "react-icons/fa";
+import { useState, useEffect } from "react";
+
+
 
 const Register = () => {
+  const [fromData, setFromData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const {name, email, password, password2} = fromData
+
+
   return (
 <>
     
@@ -9,11 +22,43 @@ const Register = () => {
       <div className="contenedorL">
       <FaUser className="registerUserL"/> 
       </div>
-
-        <div><input type="text" className="contenedorB" placeholder="Nombre"  /> </div>
-        <div><input type="email" className="contenedorB" placeholder="Email" /> </div>
-        <div><input type="password" className="contenedorB" placeholder="contraseña" /> </div>
-      <div><input type="button" className="contenedorBt" value="boton" /></div>
+{/* nombre */}
+        <div><input
+        type="text" 
+        id="name"
+        name="name"
+        value={name}
+        className="contenedorB" 
+        placeholder="Nombre"  /> </div>
+{/* email */}
+        <div><input 
+        type="email"
+        id="email" 
+        name="email"
+        value={email}
+        className="contenedorB" 
+        placeholder="Email" /> </div>
+{/* contraseña */}
+        <div><input 
+        type="password"
+        id="password" 
+        name="password"
+        value={password}
+        className="contenedorB" 
+        placeholder="contraseña" /> </div>
+{/* contraseña 2 */}
+        <div><input 
+        type="password" 
+        id="password2"
+        name="password2"
+        value={password2}
+        className="contenedorB" 
+        placeholder="Confirma tu contraseña" /> </div>
+{/* botón */}
+      <div><input 
+      type="button"
+      className="contenedorBt" 
+      value="boton" /></div>
     </div>
 </>
   )
