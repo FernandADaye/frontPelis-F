6625b5f674d3325ac56f027d878838c562c44270
Fernanda López <fer.daye.2007@gmail.com>
2024-04-19 17:28:24 -0600

    el error está en backend, pero funciona por el momento, toca arreglar el backend 😅

--- Peliculas/src/features/auth/aurhService.js
+++ Peliculas/src/features/auth/aurhService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://tan-strange-firefly.cyclic.app/users/'
+const API_URL = 'https://tan-strange-firefly.cyclic.app/users/crear'
 
 
 // peticion al backend
--- Peliculas/src/features/auth/authSlice.js
+++ Peliculas/src/features/auth/authSlice.js
@@ -14,7 +14,7 @@ const initialState ={
 // crear user 
 export const register = createAsyncThunk('auth/register', async(user , thunkAPI) => {
     try {
-        return await authService.register(user )
+        return await authService.register(user)
     }catch (error) {
         const message = (error.response && error.response.data && error.data.message) || error.message|| error.toString()
         return thunkAPI.rejectWithValue(message)
@@ -25,9 +25,9 @@ export const register = createAsyncThunk('auth/register', async(user , thunkAPI)
 
 export const authSlice = createSlice({
     name: 'auth ', 
-    initialState, 
+    initialState,  
     reducers: {
-        reset: (state )=>{
+        reset: (state)=>{
             state.isLoading= false
             state.isError= false 
             state.isSuccess= false 
@@ -37,17 +37,17 @@ export const authSlice = createSlice({
     extraReducers: (builder) =>{
         builder
         .addCase(register.pending, (state) =>{
-            state.isLoading= true
+            state.isLoading = true
         })
         .addCase( register.fulfilled, (state, action) =>{
-            state.isLoading= false
+            state.isLoading = false
             state.isSuccess = true
-            state.user=action.payload 
+            state.user = action.payload 
         })
         .addCase(register.rejected, (state, action) =>{
-            state.isLoading= false 
-            state.isError= true 
-            state.message= action.payload
+            state.isLoading = false 
+            state.isError = true 
+            state.message = action.payload
         })
     }
 })
