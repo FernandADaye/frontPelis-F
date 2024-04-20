8cb4371a5d5f3b68c0f5800197c93024578febb3
Fernanda López <fer.daye.2007@gmail.com>
2024-04-19 19:55:32 -0600

    EN PROCESOOO

--- Peliculas/src/pages/Login.jsx
+++ Peliculas/src/pages/Login.jsx
@@ -1,4 +1,9 @@
-import { useState,  } from "react";
+import { useState, useEffect } from "react";
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {reset, login } from '../features/auth/authSlice'
+import Spinner from "../components/Spinner";
 import { FaUser } from "react-icons/fa";
 
 const Login = () => {
@@ -6,14 +11,39 @@ const Login = () => {
 const [fromData, setFromData] = useState({
   email: '',
   password: '',
-
 })
-
 const {email, password} = fromData
+
+const navigate = useNavigate()
+const dispatch = useDispatch()
+
+const {user, isLoading, isError, isSuccess, message} =useSelector((state) => state.auth)  
+
+useEffect(()=>{
+  if(isError){
+    toast.error(message)
+  }
+  if(isSuccess || user){
+    navigate('/')
+  }
+  dispatch(reset())
+
+}, [user, isError, isSuccess, message, navigate, dispatch])
+
 const onSubmit= (e)=>{
   e.preventDefault()
+
+  const userData = {
+    email,
+    password
+  }
+  dispatch(login(userData))
 }
 
+if(isLoading){
+  return <Spinner/>
+
+}
 
 const onChange = (e)=>{
   setFromData((prevState) =>({
