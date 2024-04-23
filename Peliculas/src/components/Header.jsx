6c383a0da8faaa52a915ac40c3f38a4ad36c2e87
Fernanda López <fer.daye.2007@gmail.com>
2024-04-22 19:42:39 -0600

    LogOut finalizado 💩

--- Peliculas/src/components/Header.jsx
+++ Peliculas/src/components/Header.jsx
@@ -1,8 +1,22 @@
 import { FaUser,  FaUserPlus, FaHome } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 import imagen from "/assets/letra-p.png";
 
 const Header = () => {
+
+  const dispatch = useDispatch()
+  const navigate= useNavigate()
+
+  const {user} = useSelector((state) => state.auth)
+
+  const onLogout = () =>{
+dispatch(logout())
+dispatch(reset())
+navigate('/login')
+  }
+
   return (
     <>
         <header>
@@ -14,18 +28,29 @@ const Header = () => {
 
       <input className="contenedorB" type="search" id="s" value="" placeholder="Buscar"  />
 
-        <div>
+    <div className="headerContenedorRutesLi">
+      <div>
           <Link className="contenedorA" to='/'> <FaHome/> Inicio </Link>
       </div>
-      
-
-      <div>
-          <Link className="contenedorA" to='/login'> <FaUser/> Login </Link>
-        </div>
-      
-      <div >
-      <Link className="contenedorA" to='/register'> <FaUserPlus/> Registrer </Link>
-        </div>
+  
+      {user ?(
+      <>
+      <div className="contenedorA" onClick={onLogout}>
+      Login Out
+      </div>
+      </>
+  ):(
+    <>
+    <div>
+      <Link className="contenedorA" to='/login'> <FaUser/> Login </Link>
+    </div>
+  
+  <div >
+  <Link className="contenedorA" to='/register'> <FaUserPlus/> Registrer </Link>
+    </div>
+    </>
+  )}
+    </div>
       </div>
         </header>
 </>
